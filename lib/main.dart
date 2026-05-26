313feeb545a6818cc2e798146094a476787ebc30
RamdhanMahfuzh <ramdhanmahfuzh74@gmail.com>
2026-05-26 16:48:57 +0700

    remove debug

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:recipe_app/core/router/app_router.dart';
 import 'package:hive_ce_flutter/hive_flutter.dart';
 import 'package:recipe_app/features/recipe/data/models/recipe_bookmark_model.dart';
 import 'package:recipe_app/injection.dart';
-import 'package:recipe_app/test_hive.dart';
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
