e6e6e9b830682a51e22d327d0dd70d0085c2e460
RamdhanMahfuzh <ramdhanmahfuzh74@gmail.com>
2026-06-30 14:12:12 +0700

    fix build gradle

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -19,16 +19,15 @@ android {
         jvmTarget = JavaVersion.VERSION_17.toString()
     }
 
-    defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.recipe_app"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
-    }
+ defaultConfig {
+    applicationId = "com.example.recipe_app"
+
+    minSdk = 21
+    targetSdk = flutter.targetSdkVersion
+
+    versionCode = flutter.versionCode
+    versionName = flutter.versionName
+}
 
     buildTypes {
         release {
